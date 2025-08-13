ab9609680f09ea8ee87fd1bc51a4ce1d3d893307
tetraloba <four.leaves.siva7342@gmail.com>
2025-08-13 12:20:48 +0900

    jiho_mix_short

--- jiho.bash
+++ jiho.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
-# $1 (argument 1) is HOUR to echo.
 NAME="tsumugi";
-DIR="/home/tetraloba/jiho/${NAME}";
+DIR="/home/tetraloba/jiho";
 DEVICE="plughw:1,0"
 
-function play () {
+# you should call this command at HH:59 (NOT HH:00!)
+# $1 (argument 1) is HOUR to echo.
+function play_nico() {
     if test $# -ne 1; then
 	echo "invalid arguments";
 	exit;
@@ -18,18 +19,48 @@ function play () {
     fi;
     HOUR=$(($1 % 12));
 
-    aplay -D ${DEVICE} ${DIR}/nico.wav;
-    aplay -D ${DEVICE} ${DIR}/name.wav;
-    aplay -D ${DEVICE} ${DIR}/${SEQ}.wav;
-    aplay -D ${DEVICE} ${DIR}/${HOUR}.wav;
-    aplay -D ${DEVICE} ${DIR}/shirase.wav;
+    sleep 46;
+    aplay -D ${DEVICE} ${DIR}/${NAME}/nico.wav;
+    aplay -D ${DEVICE} ${DIR}/${NAME}/name.wav;
+    aplay -D ${DEVICE} ${DIR}/${NAME}/${SEQ}.wav;
+    aplay -D ${DEVICE} ${DIR}/${NAME}/${HOUR}.wav;
+    aplay -D ${DEVICE} ${DIR}/${NAME}/shirase.wav;
     sleep 1;
-    aplay -D ${DEVICE} ${DIR}/jiho.wav;
+    aplay -D ${DEVICE} ${DIR}/${NAME}/jiho.wav;
     exit;
 }
 
-# call this command at HH:59 (NOT HH:00!)
-sleep 46;
-play $(( (`date "+%-H"` + 1) % 24 ));
+# $1 (argument 1) is HOUR to echo.
+function play_short() {
+    if test $# -ne 1; then
+	echo "invalid arguments";
+	exit;
+    fi;
+
+    HOUR=$1;
+    NUM=$(printf "%03d" $((HOUR + 1)))
+
+    aplay -D ${DEVICE} ${DIR}/mix_short/${HOUR}.wav;
+    exit;
+}
+
+
+TYPE="nico"; # default
+
+while getopts t:r OPT; do
+    case $OPT in
+        t) TYPE=$OPTARG ;; # nico or short
+        r) RANDOM=1 ;;
+        *) exit 1 ;;
+    esac
+done
+
+
+case $TYPE in
+    nico) play_nico $(( (`date "+%-H"` + 1) % 24 )) ;;
+    short) play_short $(( (`date "+%-H"`) % 24 )) ;;
+    *) echo "TYPE ${TYPE} not exist." ;;
+esac
+
 exit;
 
